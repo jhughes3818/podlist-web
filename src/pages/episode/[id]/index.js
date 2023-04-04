fc110450cc13bcbbaaf1f5afe93f445e3e3cd2cb
Jason <jhughes3818@gmail.com>
2023-04-04 13:11:11 +1000

    fix styling

--- src/pages/episode/[id]/index.js
+++ src/pages/episode/[id]/index.js
@@ -28,42 +28,43 @@ export default function Episode({ episode }) {
             </a>
             .
           </p>
-        </div>
-        <div className="w-96 items-center justify-center rounded-lg border-b-8 border-l-2 border-r-8 border-t-2 border-black p-5">
-          <img src={episode.image} className="h-50 w-50 mx-auto rounded-lg" />
-          <h1 className="mt-2 text-2xl font-bold">{episode.title}</h1>
-          <p className="text-gray-600">{episode.show}</p>
-          <div className="mt-2 flex flex-row gap-2">
-            <div className="w-max rounded-lg border-2 border-black px-2 py-1">
-              <a href={episode.spotifyURL}>
-                <div>
-                  <img
-                    src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
-                    className="h-8"
-                  ></img>
-                </div>
-              </a>
-            </div>
-            {episode.appleURL ? (
-              <div className=" rounded-lg border-2 border-black px-2 py-1">
-                <a href={episode.appleURL}>
-                  <div className="flex flex-row">
+
+          <div className="w-96 items-center justify-center rounded-lg border-b-8 border-l-2 border-r-8 border-t-2 border-black p-5">
+            <img src={episode.image} className="h-50 w-50 mx-auto rounded-lg" />
+            <h1 className="mt-2 text-2xl font-bold">{episode.title}</h1>
+            <p className="text-gray-600">{episode.show}</p>
+            <div className="mt-2 flex flex-row gap-2">
+              <div className="w-max rounded-lg border-2 border-black px-2 py-1">
+                <a href={episode.spotifyURL}>
+                  <div>
                     <img
-                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Podcasts_%28iOS%29.svg/1920px-Podcasts_%28iOS%29.svg.png"
+                      src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Black.png"
                       className="h-8"
                     ></img>
-                    <div className="ml-3">
-                      <p className="text-sm text-gray-500 mb-0 p-0 leading-none font-semibold">
-                        Listen on
-                      </p>
-                      <p className="text-md font-bold mt-0 p-0 leading-none">
-                        Apple Podcasts
-                      </p>
-                    </div>
                   </div>
                 </a>
               </div>
-            ) : null}
+              {episode.appleURL ? (
+                <div className=" rounded-lg border-2 border-black px-2 py-1">
+                  <a href={episode.appleURL}>
+                    <div className="flex flex-row">
+                      <img
+                        src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Podcasts_%28iOS%29.svg/1920px-Podcasts_%28iOS%29.svg.png"
+                        className="h-8"
+                      ></img>
+                      <div className="ml-3">
+                        <p className="text-sm text-gray-500 mb-0 p-0 leading-none font-semibold">
+                          Listen on
+                        </p>
+                        <p className="text-md font-bold mt-0 p-0 leading-none">
+                          Apple Podcasts
+                        </p>
+                      </div>
+                    </div>
+                  </a>
+                </div>
+              ) : null}
+            </div>
           </div>
         </div>
       </div>
