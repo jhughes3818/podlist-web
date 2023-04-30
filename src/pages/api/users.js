c255cb25ea9486421cd6f20e43b8a8dcfde6a3e1
Jason <jhughes3818@gmail.com>
2023-04-30 17:06:30 +1000

    lift limit on api call

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -2,7 +2,11 @@ import supabase from "../../../utils/supabase";
 import { clerkClient } from "@clerk/nextjs/server";
 
 export default async function handler(req, res) {
-  const users = await clerkClient.users.getUserList();
+  const users = await clerkClient.users.getUserList({
+    limit: 100,
+  });
+
+  console.log(users.length);
 
   res.status(200).json(users);
 }
