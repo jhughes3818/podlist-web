50ced625d265c40bb7ff82e7d3fdf85cde87bd97
Jason <jhughes3818@gmail.com>
2023-05-02 16:52:43 +1000

    add user avatar endpoint

---
+++ src/pages/api/user/avatars/[id].js
@@ -0,0 +1,18 @@
+import { clerkClient } from "@clerk/nextjs/server";
+
+export default async function handler(req, res) {
+  // Get the user id from the request
+  const { id } = req.query;
+
+  // Get the user from clerk
+  const user = await clerkClient.users.getUser(id);
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  console.log(user.profileImageUrl);
+
+  // Return profileImageURL
+  res.status(200).json(user.profileImageUrl);
+}
